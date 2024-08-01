901dc851372cb92d8541526b304087eccb079bbe
Joao Santos <joaogabriel.silvasantos10@gmail.com>
2024-07-31 21:55:21 -0300

    Creatinf Post entity with nested User

---
+++ src/main/java/com/joaosantos/projectmongo/repositories/PostRepository.java
@@ -0,0 +1,11 @@
+package com.joaosantos.projectmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.joaosantos.projectmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends  MongoRepository<Post, String>{
+
+}
